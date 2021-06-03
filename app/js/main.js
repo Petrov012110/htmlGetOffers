0c3a67b37708cea4ffbc5820626770d94dfb685c
gpetrov <g.petrov@artsofte.ru>
2021-06-03 10:34:39 +0500

    footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
     $('.single-item').slick({
-        arrows: true
+        arrows: false,
+        autoplay: true,
+        speed: 900,
+        fade: true,
+        autoplaySpeed: 5000,
     });
 });
